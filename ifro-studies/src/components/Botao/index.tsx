accf0cd0be3f18b5024cac84b7793457e40db268
Gabriel Evaristo Silva <gabrielevaristo7@hotmail.com>
2022-11-16 22:01:49 -0400

    dia 16/11/2022

--- ifro-studies/src/components/Botao/index.tsx
+++ ifro-studies/src/components/Botao/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import style from './Botao.module.scss'
+import React from 'react';
+import style from './Botao.module.scss';
 
-class Botao extends React.Component<{ children?: React.ReactNode }> {
+class Botao extends React.Component <any, {
+  type?: "button" | "submit" | "reset" | undefined
+  onClick?: () => void
+}> {
   render() {
+    const { type = "button", onClick } = this.props;
     return (
-      <button className={style.botao}>
+      <button onClick={onClick} type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
